fc09e65be3b465ac32232dcf1bd183965cb623de
maximdave <enoragbondavid35@gmail.com>
2022-02-27 02:14:55 +0100

    Add project files.

---
+++ ConditionalAssign/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.Write("Please Enter Your Score : ");
+
+
+int yourScore = Convert.ToInt32(Console.ReadLine());
+
+
+if (yourScore >= 10 && yourScore <= 30)
+{
+    Console.WriteLine("Fail");
+}
+else if (yourScore >= 40 && yourScore <= 60)
+{
+    Console.WriteLine("Credit");
+}
+else if (yourScore >= 70 && yourScore <= 80)
+{
+    Console.WriteLine("Distinction");
+}
+else
+{
+    Console.WriteLine("You can only score between 10-30, 40-60, 70-80");
+}
